4932f41cf58afacedf9e52e33743a66c9c35dba9
Jeremy Heiler <jeremyheiler@gmail.com>
2017-03-09 00:25:52 -0500

    Quicksort: Add Hoare's implementation and refactor Lomuto's

--- src/algos/Quicksort.java
+++ src/algos/Quicksort.java
@@ -11,38 +11,62 @@ public class Quicksort {
         array[j] = t;
     }
 
-    private static int partition(int[] array, int lo, int hi) {
-        int pivot = array[hi], i = lo - 1;
-        for (int j = lo; j < hi; ++j) {
-            if (array[j] <= pivot) {
-                swap(array, ++i, j);
+    private static class Lomuto {
+        private int partition(int[] array, int lo, int hi) {
+            int pivot = array[hi], i = lo - 1;
+            for (int j = lo; j < hi; ++j) {
+                if (array[j] <= pivot) {
+                    swap(array, ++i, j);
+                }
+            }
+            swap(array, i + 1, hi);
+            return i + 1;
+        }
+
+        private void sort(int[] array, int lo, int hi) {
+            if (lo < hi) {
+                int p = partition(array, lo, hi);
+                sort(array, lo, p - 1);
+                sort(array, p + 1, hi);
             }
         }
-        swap(array, i + 1, hi);
-        return i + 1;
-    }
 
-    private static void sort(int[] array, int lo, int hi) {
-        if (lo < hi) {
-            int p = partition(array, lo, hi);
-            sort(array, lo, p - 1);
-            sort(array, p + 1, hi);
+        private void sort(int[] array) {
+            sort(array, 0, array.length - 1);
         }
     }
 
-    private static void sort(int[] array) {
-        sort(array, 0, array.length - 1);
+    private static class Hoare {
+        private int partition(int[] array, int lo, int hi) {
+            int pivot = array[lo], i = lo - 1, j = hi + 1;
+            for (;;) {
+                do {
+                    ++i;
+                } while (array[i] < pivot);
+                do {
+                    --j;
+                } while (array[j] > pivot);
+                if (i >= j) {
+                    return j;
+                }
+                swap(array, i, j);
+            }
+        }
+
+        private void sort(int[] array, int lo, int hi) {
+            if (lo < hi) {
+                int p = partition(array, lo, hi);
+                sort(array, lo, p);
+                sort(array, p + 1, hi);
+            }
+        }
+
+        private void sort(int[] array) {
+            sort(array, 0, array.length - 1);
+        }
     }
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = random.nextInt(1000);
-        }
-        System.out.println("Random: " + Arrays.toString(numbers));
-        sort(numbers);
-        System.out.println("Sorted: " + Arrays.toString(numbers));
+    private static void check(int[] numbers) {
         int last = 0;
         boolean valid = true;
         for (int number : numbers) {
@@ -56,5 +80,22 @@ public class Quicksort {
         if (valid) {
             System.out.println("The array is sorted");
         }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers1 = new int[10];
+        int[] numbers2 = new int[10];
+        for (int i = 0; i < numbers1.length; ++i) {
+            numbers1[i] = random.nextInt(1000);
+        }
+        System.arraycopy(numbers1, 0, numbers2, 0, numbers1.length);
+        System.out.println("Random: " + Arrays.toString(numbers1));
+        new Lomuto().sort(numbers1);
+        System.out.print("Lomuto: " + Arrays.toString(numbers1) + " ");
+        check(numbers1);
+        new Hoare().sort(numbers2);
+        System.out.print("Hoare:  " + Arrays.toString(numbers2) + " ");
+        check(numbers2);
     }
 }
